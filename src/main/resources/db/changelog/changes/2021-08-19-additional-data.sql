9d76b908390b42e1913f3637cd93888ae7ba6f4d
Dovydas Radzys <radzysdovydas2@gmail.com>
2021-08-19 17:11:19 +0300

    additional dummy data

---
+++ src/main/resources/db/changelog/changes/2021-08-19-additional-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO comment(uuid, comment, post_uuid)
+VALUES ('28612604-abc9-42f2-b257-0fe34d0bd2f9', 'random comment',
+        '4e3668a3-ac58-406d-96f6-ed9a52c37e70'),
+       ('e44a23be-cbaa-4175-a8c9-c31723adb303', 'comment', 'dbb7308c-fb1b-402b-9096-e0274448e0c8'),
+       ('e44ad3be-c1a2-4175-a8c9-c3172aadb303', 'lorem ipsum',
+        'd49ee33f-6962-4b4c-9bad-62ec2e195ad2');
